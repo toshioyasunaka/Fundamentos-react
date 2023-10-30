00ab34131d568b4ecaba8f0535b3ca838dee3959
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2023-10-30 06:18:02 -0300

    create Comment Form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,6 +6,12 @@ import { Avatar } from './Avatar';
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
+const comments = [
+  1,
+  2,
+  3,
+]
+
 export function Post({ author, publishedAt, content }) {
   const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -45,10 +51,22 @@ export function Post({ author, publishedAt, content }) {
       </div>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment, index) => {
+          return <Comment key={index}/>
+        })}
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
